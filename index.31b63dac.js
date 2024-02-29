4e0f32139510c11415986524ca9ce03c2ff6d169
Davwidos <d.jurkowski015@gmail.com>
2024-02-29 13:25:29 +0100

    make build

---
+++ index.31b63dac.js
@@ -0,0 +1,3 @@
+var e,t=(e=document.getElementById("largeImg"),function(t){t.preventDefault();for(var r=t.target;"A"!==r.tagName;)if(!(r=r.parentNode))throw Error('not inside "a" tag');e.setAttribute("src",r.href)});document.getElementById("thumbs").querySelectorAll("a, img").forEach(function(e){return e.addEventListener("click",t)});//# sourceMappingURL=index.31b63dac.js.map
+
+//# sourceMappingURL=index.31b63dac.js.map
